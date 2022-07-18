1bd7f45b508203941d5c0f670ab3af7b71de87d8
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-07-18 17:49:30 +0200

    update

--- main.tf
+++ main.tf
@@ -128,4 +128,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   boot_diagnostics {
     storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
   }
+ /*   provisioner "local-exec" {
+    command = "ansible-playbook -u user -i '${self.public_ip},' --private-key ${var.ssh_key_private} provision.yml" 
+  } */
 }
